62e8ebdc2c91d5d80d5739fd5fd666f565e2128f
Arigitshub <134266911+Arigitshub@users.noreply.github.com>
2025-10-04 23:57:23 -0400

    Add controller_support.py template with enable_controller_support function

--- src/controller_support.py
+++ src/controller_support.py
@@ -1,1 +1,9 @@
-# Full controller support functionality
+"""Full controller support functionality."""
+
+def enable_controller_support():
+    """Enable full controller support for the game."""
+    print("Enabling controller support...")
+    # TODO: Implement controller detection and mapping
+    # TODO: Add button configuration
+    # TODO: Add analog stick support
+    pass
